753c6b5bfab9f6bd107d49da455e429af4daefbc
Pieter Libin <pieter.libin@rega.kuleuven.be>
2013-11-20 21:22:52 +0100

    add utility method to convert a map to a string, this string can be used for invoking restful web services (since it url encodes its values)

--- src/com/github/drinking_buddies/webservices/rest/RestRequest.java
+++ src/com/github/drinking_buddies/webservices/rest/RestRequest.java
@@ -1,6 +1,10 @@
 package com.github.drinking_buddies.webservices.rest;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Map;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.ParseException;
@@ -35,6 +39,20 @@ public class RestRequest {
 				httpClient.getConnectionManager().shutdown();;
 			}
 		}
+	}
+	
+	public static String argMapToString(Map<String, String> args) {
+	    StringBuilder sb = new StringBuilder();
+	    for (String k : args.keySet()) {
+	       if (sb.length() > 0)
+	           sb.append("&");
+	       try {
+	           sb.append(k + "=" + URLEncoder.encode(args.get(k), "UTF8"));
+           } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+           }
+	    }
+	    return sb.toString();
 	}
 
 }
