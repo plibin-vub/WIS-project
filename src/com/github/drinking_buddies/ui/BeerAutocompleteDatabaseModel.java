d3b1fd3624f6445cf8cfea958baa4e52230e2af4
Pieter Libin <pieter.libin@rega.kuleuven.be>
2013-11-23 10:16:52 +0100

    implement the beer autocomplete db model

--- src/com/github/drinking_buddies/ui/BeerAutocompleteDatabaseModel.java
+++ src/com/github/drinking_buddies/ui/BeerAutocompleteDatabaseModel.java
@@ -1,7 +1,17 @@
 package com.github.drinking_buddies.ui;
 
+import static com.github.drinking_buddies.jooq.Tables.BEER;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Record1;
+import org.jooq.Result;
+
+import com.github.drinking_buddies.Application;
 import com.github.drinking_buddies.ui.autocompletion.AutocompleteDatabaseModel;
 
 public class BeerAutocompleteDatabaseModel extends AutocompleteDatabaseModel {
@@ -9,15 +19,26 @@ public class BeerAutocompleteDatabaseModel extends AutocompleteDatabaseModel {
         super(limit);
     }
 
-    static String [] lijst = {"duvel", "duvel tripel hop", "westmalle tripel", "westmalle dobbel", "leffe blond", "leffe bruin", "leffe radieus"};
-    
     @Override
     public void filterQuery(String s, int limit, ArrayList<String> list) {
-        for (String ss : lijst) {
-            if (ss.startsWith(s))
-                list.add(ss);
+        try {
+            Application app = Application.getInstance();
+            Connection conn = app.getConnection();
+            DSLContext dsl = app.createDSLContext(conn);
+            Result<Record1<String>> results = 
+                    dsl
+                        .select(BEER.NAME)
+                        .from(BEER)
+                        .where(BEER.NAME.startsWith(s))
+                        .orderBy(BEER.NAME)
+                        .limit(limit)
+                        .fetch();
+            
+            for (Record r : results) {
+                list.add(r.getValue(BEER.NAME));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        if (list.size() == 1)
-            list.clear();
     }
 }
