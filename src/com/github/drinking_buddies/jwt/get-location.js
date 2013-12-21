dcfd102ed8a597fea65ee290d0865129506f8dd5
ids_dbt <ids_dbt@DLLNB1501.DLL.infra>
2013-12-21 11:10:36 +0100

    format javascript

--- src/com/github/drinking_buddies/jwt/get-location.js
+++ src/com/github/drinking_buddies/jwt/get-location.js
@@ -1,15 +1,18 @@
 getLocation();
+
 function getLocation() {
-	if (navigator.geolocation)
-	{
+	if (navigator.geolocation){
 		navigator.geolocation.getCurrentPosition(returnPosition,returnError);
 	}
-	else{returnError();}
+	else{
+		returnError();
+	}
 }
+
 function returnPosition(position){
 	Wt.emit(Wt, {name: '${signal-name}', event: null, eventObject: null}, position.coords.latitude,position.coords.longitude); 
 }
-function returnError(position)
-{
+
+function returnError(position){
 	Wt.emit(Wt, {name: '${signal-name}', event: null, eventObject: null},50.8833 ,4.7000); 
 }
